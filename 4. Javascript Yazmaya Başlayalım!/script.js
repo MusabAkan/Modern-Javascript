9c49700bdd42942cfbf5482104bd37b84edf75d3
Musab Akan <104889820+MusabAkan@users.noreply.github.com>
2022-08-30 22:02:44 +0300

    #3008222202

--- 4. Javascript Yazmaya Başlayalım!/script.js
+++ 4. Javascript Yazmaya Başlayalım!/script.js
@@ -1,1 +1,7 @@
-console.log("merhaba");
+var maaşAli = 8500;
+var maasCan = 6000;
+
+var zam = 0.3
+console.log(maasAli + maasAli * zam); // maaş ali
+console.log(maasCan + maasCan * zam); //maaş can
+    
