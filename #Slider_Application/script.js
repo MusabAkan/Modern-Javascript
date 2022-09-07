0994f2032fbb82b4c0efc3ca40ada0a3e26f32f3
Musab Akan <104889820+MusabAkan@users.noreply.github.com>
2022-09-07 22:12:22 +0300

    #

--- #Slider_Application/script.js
+++ #Slider_Application/script.js
@@ -28,14 +28,68 @@ var models = [
 
 var index = 0;
 var slaytCount = models.length;
+var interval;
 
-showSlide(index);
+var settings = {
+  duration: "1000",
+  random: false,
+};
+
+init(settings);
+
+document
+  .querySelector(".fa-arrow-circle-left")
+  .addEventListener("click", function () {
+    index--;
+    showSlide(index);
+  });
+
+document
+  .querySelector(".fa-arrow-circle-right")
+  .addEventListener("click", function () {
+    index++;
+    showSlide(index);
+  });
+
+document.querySelectorAll(".arrow").forEach(function (item) {
+  item.addEventListener("mouseenter", function () {
+    clearInterval(interval);
+  });
+});
+
+document.querySelectorAll(".arrow").forEach(function (item) {
+  item.addEventListener("mouseleave", function () {
+    init(settings);
+  });
+});
+
+function init(settings) {
+  var prev;
+  interval = setInterval(function () {
+    if (settings.random) {
+      // random index
+      do {
+        index = Math.floor(Math.random() * slaytCount);
+      } while (index == prev);
+      prev = index;
+    } else {
+      // artan index
+      if (slaytCount == index + 1) {
+        index = -1;
+      }
+      showSlide(index);
+      index++;
+    }
+    showSlide(index);
+  }, settings.duration);
+}
 
 function showSlide(i) {
+  index = i;
+
   if (i < 0) {
     index = slaytCount - 1;
   }
-
   if (i >= slaytCount) {
     index = 0;
   }
@@ -48,17 +102,3 @@ function showSlide(i) {
 
   document.querySelector(".card-link").setAttribute("href", models[index].link);
 }
-
-document
-  .querySelector(".fa-arrow-circle-right")
-  .addEventListener("click", function () {
-    index++;
-    showSlide(index);
-  });
-
-document
-  .querySelector(".fa-arrow-circle-left")
-  .addEventListener("click", function () {
-    index--;
-    showSlide(index);
-  });
