6bf74e3cca06c72ba29603c9a483bfd1c3a4b189
Musab Akan <104889820+MusabAkan@users.noreply.github.com>
2022-09-13 22:34:04 +0300

    #1309222234

---
+++ #Quiz_Application_OOP/script.js
@@ -0,0 +1,81 @@
+//OOP : Nesne Tabanlı Progralama
+//object
+
+function Soru(soruMetni, cevapSecenekleri, dogruCevap) {
+  this.soruMetni = soruMetni;
+  this.cevapSecenekleri = cevapSecenekleri;
+  this.dogruCevap = dogruCevap;
+}
+
+Soru.prototype.cevabiKontrolEt = function (cevap) {
+  return cevap === this.dogruCevap;
+  /*sürekli çalıştırmamak adına yapılmıtır.
+   */
+};
+
+let soru1 = new Soru(
+  "Hangisi javascript paket yönetim  uygulamasıdır ?",
+  { a: "Node.js", b: "Typescript", c: "Npm" },
+  "c"
+);
+let soru2 = new Soru(
+  "Hangisi .net paket yönetim  uygulamasıdır ?",
+  { a: "Node.js", b: "nuget", c: "Npm" },
+  "b"
+);
+
+// yada dizi halinde atabilirsin
+
+let sorular = [
+  new Soru(
+    "1-Hangisi javascript paket yönetim  uygulamasıdır ?",
+    { a: "Node.js", b: "Typescript", c: "Npm" },
+    "c"
+  ),
+  new Soru(
+    "2-Hangisi javascript paket yönetim  uygulamasıdır ?",
+    { a: "Node.js", b: "Typescript", c: "Npm" },
+    "c"
+  ),
+  new Soru(
+    "3-Hangisi javascript paket yönetim  uygulamasıdır ?",
+    { a: "Node.js", b: "Typescript", c: "Npm" },
+    "c"
+  ),
+  new Soru(
+    "4-Hangisi javascript paket yönetim  uygulamasıdır ?",
+    { a: "Node.js", b: "Typescript", c: "Npm" },
+    "c"
+  ),
+];
+
+
+function Quiz(sorular) {
+    this.sorular = sorular;
+    this.soruIndex = 0;
+}
+
+Quiz.prototype.soruGetir = function () {
+    return this.sorular[this.soruIndex];
+};
+
+const quiz = new Quiz(sorular);
+
+
+document.querySelector(".btn-start").addEventListener("click", function(){
+    if (quiz.sorular.length != quiz.soruIndex) {
+        console.log(quiz.soruGetir())
+        quiz.soruIndex+=1    
+    }
+    else{
+        console.log("Sorularınız bitti")
+    }
+
+
+    
+})
+
+// console.log(quiz.soruGetir())
+// quiz.soruIndex+=1
+
+// console.log(quiz.soruGetir())
