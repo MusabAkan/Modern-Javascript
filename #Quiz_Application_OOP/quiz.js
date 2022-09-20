76e64b4a191fe87ecb6c25edaf60c4c57c2d32e8
Musab Akan <104889820+MusabAkan@users.noreply.github.com>
2022-09-20 14:41:58 +0300

    #2009221441

--- #Quiz_Application_OOP/quiz.js
+++ #Quiz_Application_OOP/quiz.js
@@ -1,10 +1,9 @@
-class Quiz {
-  constructor(sorular) {
+function Quiz(sorular) {
     this.sorular = sorular;
     this.soruIndex = 0;
-  }
-  soruGetir() {
-    return this.sorular[this.soruIndex];
-  }
+    this.dogruCevapSayisi = 0;
 }
 
+Quiz.prototype.soruGetir = function() {
+    return this.sorular[this.soruIndex];
+}
